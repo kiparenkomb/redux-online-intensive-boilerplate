8edf28a83dd279fe3fbb047d08c5612c8f74585e
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2018-08-03 17:17:04 +0300

    Added createLogger and Redux devtools

--- source/basic-redux/init/store.js
+++ source/basic-redux/init/store.js
@@ -1,7 +1,23 @@
 // Core
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 // Reducer
 import { rootReducer } from './rootReduser';
 
-export const store = createStore(rootReducer);
+const logger = createLogger({
+    duration:  true,
+    collapsed: true,
+    colors:    {
+        title:     () => '#139BFE',
+        prevState: () => '#1C5FAF',
+        action:    () => '#149945',
+        nextState: () => '#A47104',
+        error:     () => '#FF0005',
+    },
+});
+
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools ? devtools : compose;
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
